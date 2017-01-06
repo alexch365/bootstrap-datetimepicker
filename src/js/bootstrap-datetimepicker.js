05b64c6024c3db0f53a5b7a6807d89ba8ae0eff0
alex365 <alexch365@yandex.ru>
2017-01-06 22:13:14 +0400

    fix place function in bootstrap-datetimepicker.js

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -403,74 +403,77 @@
             },
 
             place = function () {
-                var position = (component || element).position(),
-                    offset = (component || element).offset(),
-                    vertical = options.widgetPositioning.vertical,
-                    horizontal = options.widgetPositioning.horizontal,
-                    parent;
+               var offset = (component || element).offset(),
+                   vertical = options.widgetPositioning.vertical,
+                   horizontal = options.widgetPositioning.horizontal,
+                   parent,
+                   position;
 
-                if (options.widgetParent) {
-                    parent = options.widgetParent.append(widget);
-                } else if (element.is('input')) {
-                    parent = element.after(widget).parent();
-                } else if (options.inline) {
-                    parent = element.append(widget);
-                    return;
-                } else {
-                    parent = element;
-                    element.children().first().after(widget);
-                }
+               if (options.widgetParent) {
+                   parent = options.widgetParent.append(widget);
+               } else if (element.is('input')) {
+                   parent = element.after(widget).parent();
+               } else if (options.inline) {
+                   parent = element.append(widget);
+                   return;
+               } else {
+                   parent = element;
+                   element.children().first().after(widget);
+               }
 
-                // Top and bottom logic
-                if (vertical === 'auto') {
-                    if (offset.top + widget.height() * 1.5 >= $(window).height() + $(window).scrollTop() &&
-                        widget.height() + element.outerHeight() < offset.top) {
-                        vertical = 'top';
-                    } else {
-                        vertical = 'bottom';
-                    }
-                }
+               // Top and bottom logic
+               if (vertical === 'auto') {
+                   if (offset.top + widget.height() * 1.5 >= $(window).height() + $(window).scrollTop() &&
+                       widget.height() + element.outerHeight() < offset.top) {
+                       vertical = 'top';
+                   } else {
+                       vertical = 'bottom';
+                   }
+               }
 
-                // Left and right logic
-                if (horizontal === 'auto') {
-                    if (parent.width() < offset.left + widget.outerWidth() / 2 &&
-                        offset.left + widget.outerWidth() > $(window).width()) {
-                        horizontal = 'right';
-                    } else {
-                        horizontal = 'left';
-                    }
-                }
+               // Left and right logic
+               if (horizontal === 'auto') {
+                   if (element.width() < offset.left + widget.outerWidth() / 2 &&
+                       offset.left + widget.outerWidth() > $(window).width()) {
+                       horizontal = 'right';
+                   } else {
+                       horizontal = 'left';
+                   }
+               }
 
-                if (vertical === 'top') {
-                    widget.addClass('top').removeClass('bottom');
-                } else {
-                    widget.addClass('bottom').removeClass('top');
-                }
+               if (vertical === 'top') {
+                   widget.addClass('top').removeClass('bottom');
+               } else {
+                   widget.addClass('bottom').removeClass('top');
+               }
 
-                if (horizontal === 'right') {
-                    widget.addClass('pull-right');
-                } else {
-                    widget.removeClass('pull-right');
-                }
+               if (horizontal === 'right') {
+                   widget.addClass('pull-right');
+               } else {
+                   widget.removeClass('pull-right');
+               }
 
-                // find the first parent element that has a relative css positioning
-                if (parent.css('position') !== 'relative') {
-                    parent = parent.parents().filter(function () {
-                        return $(this).css('position') === 'relative';
-                    }).first();
-                }
+               // find the first parent element that has a relative css positioning
+               if (parent.css('position') !== 'relative') {
+                   parent = parent.parents().filter(function () {
+                       return $(this).css('position') === 'relative';
+                   }).first();
+               }
 
-                if (parent.length === 0) {
-                    throw new Error('datetimepicker component should be placed within a relative positioned container');
-                }
+               if (parent.length === 0) {
+                   throw new Error('datetimepicker component should be placed within a relative positioned container');
+               }
 
-                widget.css({
-                    top: vertical === 'top' ? 'auto' : position.top + element.outerHeight(),
-                    bottom: vertical === 'top' ? parent.outerHeight() - (parent === element ? 0 : position.top) : 'auto',
-                    left: horizontal === 'left' ? (parent === element ? 0 : position.left) : 'auto',
-                    right: horizontal === 'left' ? 'auto' : parent.outerWidth() - element.outerWidth() - (parent === element ? 0 : position.left)
-                });
-            },
+               position = {
+                   left: (component || element).offset().left - $(parent).offset().left,
+                   top: (component || element).offset().top - $(parent).offset().top
+               };
+
+               widget.css({
+                   top: vertical === 'top' ? position.top - (component || element).height()/2 - widget.outerHeight() : position.top + (component || element).outerHeight(),
+                   left: horizontal === 'left' ? position.left : position.left + (component || element).outerWidth() - widget.outerWidth()
+               });
+           },
 
             notifyEvent = function (e) {
                 if (e.type === 'dp.change' && ((e.date && e.date.isSame(e.oldDate)) || (!e.date && !e.oldDate))) {
